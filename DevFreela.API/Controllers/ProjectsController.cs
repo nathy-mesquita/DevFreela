afa3dea3a5409bf1a544f6b44e7977e639a33f13
Maria Nathaly Gomes de Mesquita <Maria.Mesquita@ativainvestimentos.com.br>
2021-06-27 15:40:40 -0300

    first commit

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
