4d0ab491ffd55f2fcdb4b377b93cd5f1ac043e82
Maria Nathaly Gomes de Mesquita <Maria.Mesquita@ativainvestimentos.com.br>
2021-06-29 10:12:33 -0300

    feat(OpeningTimeOption): Configurações da aplicação

---
+++ DevFreela.API/Models/OpeningTimeOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace DevFreela.API.Models
+{
+    public class OpeningTimeOption
+    {
+        public TimeSpan StartAt { get; set; }
+        public TimeSpan FinishAt { get; set; }
+    }
+}
