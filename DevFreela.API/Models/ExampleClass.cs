52d16a0b570da314e8854d0a0205af6c0b653dd3
Maria Nathaly Gomes de Mesquita <Maria.Mesquita@ativainvestimentos.com.br>
2021-06-30 10:35:52 -0300

    feat(ExampleClass): classe para demonstração da Injeção de dependência

---
+++ DevFreela.API/Models/ExampleClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace DevFreela.API.Models
+{
+    public class ExampleClass
+    {
+        public string Name { get; set; }
+    }
+}
