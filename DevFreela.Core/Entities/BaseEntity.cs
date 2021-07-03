44adb6de4b76b16069288bbeef518a0b87703f73
Maria Nathaly Gomes de Mesquita <Maria.Mesquita@ativainvestimentos.com.br>
2021-07-03 14:41:36 -0300

    fix(): Base entities

---
+++ DevFreela.Core/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+namespace DevFreela.Core.Entities
+{
+    public abstract class BaseEntity
+    {
+        protected BaseEntity()
+        {
+            
+        }
+        public int Id { get; private set; }    
+    }
+}
