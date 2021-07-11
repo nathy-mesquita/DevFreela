749c8f215f33f39153d7ee962d236d42d3052e93
Maria Nathaly Gomes de Mesquita <Maria.Mesquita@ativainvestimentos.com.br>
2021-07-10 22:23:43 -0300

    fix(): InputModels do ApplicationService

--- DevFreela.Application/Models/InputModels/CreateCommentInputModel.cs
+++ DevFreela.Application/Models/InputModels/CreateCommentInputModel.cs
@@ -2,6 +2,8 @@ namespace DevFreela.Application.Models.InputModels
 {
     public class CreateCommentInputModel
     {
-        
+        public string Content { get; set; }
+        public int IdProject { get; set; }
+        public int IdUser { get; set; }
     }
 }
--- DevFreela.Application/Models/InputModels/NewProjectInputModel.cs
+++ DevFreela.Application/Models/InputModels/NewProjectInputModel.cs
@@ -2,6 +2,10 @@ namespace DevFreela.Application.Models.InputModels
 {
     public class NewProjectInputModel
     {
-        
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int IdCliente { get; set; }
+        public int IdFreelancer { get; set; }
+        public decimal TotalCost { get; set; }
     }
 }
