6047d60ece5be60e78967e4325edcf9887a49a03
Maria Nathaly Gomes de Mesquita <Maria.Mesquita@ativainvestimentos.com.br>
2021-07-10 22:26:48 -0300

    fix(): Implementando métodos do ProjectService

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -1,4 +1,7 @@
+using System.Linq;
+using DevFreela.Core.Entities;
 using System.Collections.Generic;
+using DevFreela.Infrastructure.Persistence;
 using DevFreela.Application.Models.InputModels;
 using DevFreela.Application.Models.ViewModels;
 using DevFreela.Application.Services.Interfaces;
@@ -7,44 +10,67 @@ namespace DevFreela.Application.Services.Implementations
 {
     public class ProjectService : IProjectService
     {
-        int IProjectService.Create(NewProjectInputModel inputModel)
-        {
-            throw new System.NotImplementedException();
-        }
+        private readonly DevFreelaDbContext _dbContext;
 
-        void IProjectService.CreateComment(CreateCommentInputModel inputModel)
-        {
-            throw new System.NotImplementedException();
-        }
+        public ProjectService(DevFreelaDbContext dbContext) 
+        => _dbContext = dbContext;
 
-        void IProjectService.Delete(int id)
+        public List<ProjectViewModel> GetAll(string query)
         {
-            throw new System.NotImplementedException();
-        }
+            //Busca todos os projetos
+            var projects = _dbContext.Projects;
 
-        void IProjectService.Finish(int id)
-        {
-            throw new System.NotImplementedException();
-        }
+            var projectViewModel = projects
+            .Select(p => new ProjectViewModel(p.Title, p.CreatedAt)) // Seleciona o projeto que foi buscado dizendo que ele é do tipo ProjectVieeModel e passa as propriedades criadas no construtor
+            .ToList(); //Gera uma lista a partir das projeções desses dados
 
-        List<ProjectViewModel> IProjectService.GetAll(string query)
-        {
-            throw new System.NotImplementedException();
+            return projectViewModel;
         }
-
-        ProjectDetailsViewModel IProjectService.GetById(int id)
+        public ProjectDetailsViewModel GetById(int id)
         {
-            throw new System.NotImplementedException();
+            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
+            var projectDetailsViewModel = new ProjectDetailsViewModel(
+                project.Id,
+                project.Title,
+                project.Description,
+                project.TotalCost,
+                project.StartedAt,
+                project.FinishedAt
+            );
+            return projectDetailsViewModel;
         }
-
-        void IProjectService.Start(int id)
+        public int Create(NewProjectInputModel inputModel)
         {
-            throw new System.NotImplementedException();
+            //! Converter um NewprojectInputModel para um Project - AutoMapper
+            //Faremos isso a princípio de maneira manual
+            var project = new Project(inputModel.Title, inputModel.Description, inputModel.IdCliente, inputModel.IdFreelancer, inputModel.TotalCost);
+            _dbContext.Projects.Add(project);
+            return project.Id;
         }
-
-        void IProjectService.Update(UpdateProjectInputModel inputModel)
+        public void Update(UpdateProjectInputModel inputModel)
         {
-            throw new System.NotImplementedException();
+            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == inputModel.Id);
+            project.Update(inputModel.Title, inputModel.Description, inputModel.TotalCost);
+        }
+        public void Delete(int id)
+        {
+            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
+            project.Cancel();
+        }
+        public void CreateComment(CreateCommentInputModel inputModel)
+        {
+            var comment = new ProjectComments(inputModel.Content, inputModel.IdProject, inputModel.IdUser);
+            _dbContext.ProjectComments.Add(comment);
+        }
+        public void Start(int id)
+        {
+            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
+            project.Start();
+        }
+        public void Finish(int id)
+        {
+            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
+            project.Finish();
         }
     }
 }
