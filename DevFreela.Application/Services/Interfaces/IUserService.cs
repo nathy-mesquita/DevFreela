2a7e5e95475d4eca1c99dc6bd536743ec970b147
Maria Nathaly Gomes de Mesquita <Maria.Mesquita@ativainvestimentos.com.br>
2021-07-13 10:04:52 -0300

    feat(IUserService): add interface

---
+++ DevFreela.Application/Services/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using DevFreela.Application.Models.ViewModels;
+using DevFreela.Application.Models.InputModels;
+
+namespace DevFreela.Application.Services.Interfaces
+{
+    public interface IUserService
+    {
+        List<UserViewModel> GetAll(string query);
+        UserDetailsViewModel GetById(int id);
+        int Create(NewUserInputModel inputModel);
+        void Update(UpdateUserInputModel inputModel);
+    }
+}
