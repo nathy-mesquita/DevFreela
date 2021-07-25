ea92758e42310a994a954542e47295d10c298fd3
Maria Nathaly Gomes de Mesquita <Maria.Mesquita@ativainvestimentos.com.br>
2021-07-25 15:50:29 -0300

    feat(UserSkillConfigurations): Add class

---
+++ DevFreela.Infrastructure/Persistence/Configurations/UserSkillConfigurations.cs
@@ -0,0 +1,16 @@
+using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevFreela.Infrastructure.Persistence.Configurations
+{
+    public class UserSkillConfigurations : IEntityTypeConfiguration<UserSkill>
+    {
+        public void Configure(EntityTypeBuilder<UserSkill> builder)
+        {
+            //Chave primária
+            builder
+            .HasKey(u => u.Id);
+        }
+    }
+}
