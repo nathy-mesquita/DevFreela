ed6fa8eab07298e064f298a299fb875835e189a3
Maria Nathaly Gomes de Mesquita <Maria.Mesquita@ativainvestimentos.com.br>
2021-07-25 15:50:01 -0300

    feat(SkillConfigurations): Add class

---
+++ DevFreela.Infrastructure/Persistence/Configurations/SkillConfigurations.cs
@@ -0,0 +1,16 @@
+using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevFreela.Infrastructure.Persistence.Configurations
+{
+    public class SkillConfigurations : IEntityTypeConfiguration<Skill>
+    {
+        public void Configure(EntityTypeBuilder<Skill> builder)
+        {
+            //Chave primária
+            builder
+            .HasKey(s => s.Id);
+        }
+    }
+}
