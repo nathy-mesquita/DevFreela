ef4f314b7c201d1513ba4f6a2490446e18c9dfac
Nathaly Mesquita <nathaly.mesquita@xpi.com.br>
2021-08-18 00:23:18 -0300

    refactor! Remove Dapper

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -7,40 +7,21 @@ using DevFreela.Application.Models.InputModels;
 using DevFreela.Application.Models.ViewModels;
 using DevFreela.Application.Services.Interfaces;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Data.SqlClient;
-using Dapper;
 
 namespace DevFreela.Application.Services.Implementations
 {
     public class ProjectService : IProjectService
     {
         private readonly DevFreelaDbContext _dbContext;
-        private readonly string _connectionString;
-
         public ProjectService(DevFreelaDbContext dbContext, IConfiguration configuration) 
-        {
-            _dbContext = dbContext;
-            _connectionString = configuration.GetConnectionString("DevFreelaCs");
-        } 
-            
-
+            => _dbContext = dbContext;
         public List<ProjectViewModel> GetAll(string query)
         {
-            // Todo: Refatorando com Dapper
-            using(var sqlConnection = new SqlConnection(_connectionString))
-            {
-                sqlConnection.Open();
-                var script = "SELECT Id, Title, CreatedAt FROM Project";
-                return sqlConnection.Query<ProjectViewModel>(script).ToList();
-            }
-
-            //! Caso esteja usando o EF Core In Memory o Dapper não funcionará
-            //Busca todos os projetos
             var projects = _dbContext.Projects;
 
             var projectViewModel = projects
-            .Select(p => new ProjectViewModel(p.Id, p.Title, p.CreatedAt)) // Seleciona o projeto que foi buscado dizendo que ele é do tipo ProjectVieeModel e passa as propriedades criadas no construtor
-            .ToList(); //Gera uma lista a partir das projeções desses dados
+            .Select(p => new ProjectViewModel(p.Id, p.Title, p.CreatedAt)) 
+            .ToList();
 
             return projectViewModel;
         }
@@ -52,32 +33,20 @@ namespace DevFreela.Application.Services.Implementations
                 .SingleOrDefault(p => p.Id == id);
             if (project == null) return null;
 
-            // Todo: Refatorando com Dapper
-            using(var sqlConnection = new SqlConnection(_connectionString))
-            {
-                sqlConnection.Open();
-                var script = "SELECT p.Id as Id, p.Title as Title, p.Description as Description, c.FullName as Client, f.FullName as Freelancer, p.TotalCost as TotalCost, p.CreatedAt as CreatedAt, p.StartedAt as StartedAt, p.FinishedAt as FinishedAt FROM Projects p INNER JOIN Users u ON u.Id = p.Id INNER JOIN Users c ON c.Id = p.IdCliente INNER JOIN Users f ON f.Id = p.IdFreelancer;";
-                var result = sqlConnection.Query<ProjectDetailsViewModel>(script);
-                return result.SingleOrDefault(p => p.Id == id);
-            }
-
-            //! Caso esteja usando o EF Core In Memory o Dapper não funcionará
-            // var projectDetailsViewModel = new ProjectDetailsViewModel(
-            //     project.Id,
-            //     project.Title,
-            //     project.Description,
-            //     project.TotalCost,
-            //     project.StartedAt,
-            //     project.FinishedAt,
-            //     project.Client.FullName,
-            //     project.Freelancer.FullName
-            // );
-            // return projectDetailsViewModel;
+            var projectDetailsViewModel = new ProjectDetailsViewModel(
+                project.Id,
+                project.Title,
+                project.Description,
+                project.TotalCost,
+                project.StartedAt,
+                project.FinishedAt,
+                project.Client.FullName,
+                project.Freelancer.FullName
+            );
+            return projectDetailsViewModel;
         }
         public int Create(NewProjectInputModel inputModel)
         {
-            //! Converter um NewprojectInputModel para um Project - AutoMapper
-            //Faremos isso a princípio de maneira manual
             var project = new Project(inputModel.Title, inputModel.Description, inputModel.IdCliente, inputModel.IdFreelancer, inputModel.TotalCost);
             _dbContext.Projects.Add(project);
             _dbContext.SaveChanges();
@@ -87,29 +56,13 @@ namespace DevFreela.Application.Services.Implementations
         {
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == inputModel.Id);
             project.Update(inputModel.Title, inputModel.Description, inputModel.TotalCost);
-            // Todo: Refatorando com Dapper
-            using(var sqlConnection = new SqlConnection(_connectionString))
-            {
-                sqlConnection.Open();
-                var script ="UPDATE Projects SET Title = @title, Description = @description, TotalCost = @totalcost WHERE Id = @id";
-                sqlConnection.Execute(script, new {title = project.Title, description = project.Description, totalcost = project.TotalCost, id = project.Id});
-            }
-            //! Utilizando EF Core
-           // _dbContext.SaveChanges();
+            _dbContext.SaveChanges();
         }
         public void Delete(int id)
         {
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
             project.Cancel();
-            // Todo: Refatorando com Dapper
-            using(var sqlConnection = new SqlConnection(_connectionString))
-            {
-                sqlConnection.Open();
-                var script = "UPDATE Projects SET Status = @status WHERE Id = @id";
-                sqlConnection.Execute(script, new {status = project.Status, id = project.Id});
-            }
-            //! Utilizando EF Core
-            //_dbContext.SaveChanges();
+            _dbContext.SaveChanges();
         }
         public void CreateComment(CreateCommentInputModel inputModel)
         {
@@ -121,29 +74,13 @@ namespace DevFreela.Application.Services.Implementations
         {
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
             project.Start();
-            // Todo: Refatorando com Dapper
-            using(var sqlConnection = new SqlConnection(_connectionString))
-            {
-                sqlConnection.Open();
-                var script = "UPDATE Projects SET Status = @status, StartedAt = @startedat WHERE Id = @id";
-                sqlConnection.Execute(script, new {status = project.Status, startedat = project.StartedAt, id});
-            }
-            //! Utilizando EF Core
-           // _dbContext.SaveChanges();
+            _dbContext.SaveChanges();
         }
         public void Finish(int id)
         {
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
             project.Finish();
-            // Todo: Refatorando com Dapper
-            using(var sqlConnection = new SqlConnection(_connectionString))
-            {
-                sqlConnection.Open();
-                var script = "UPDATE Projects SET Status = @status, FinishedAt = @finishedAt WHERE Id = @id";
-                sqlConnection.Execute(script, new {status = project.Status, finishedat = project.FinishedAt, id});
-            }
-            //! Utilizando EF Core
-            //_dbContext.SaveChanges();
+            _dbContext.SaveChanges();
         }
     }
 }
