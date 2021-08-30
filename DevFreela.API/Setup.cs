b2e176223b32187aca61e6ed7b6dff7b43bb5f5c
Nathaly Mesquita <nathaly.mesquita@xpi.com.br>
2021-08-30 01:56:11 -0300

    feat(): Setup

---
+++ DevFreela.API/Setup.cs
@@ -0,0 +1,47 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevFreela.API
+{
+    public static class Setup
+    {
+        public static void AddWebApi(this IServiceCollection services)
+        {
+            services.AddControllers()
+                    .AddJsonOptions(options =>
+                    {
+                        options.JsonSerializerOptions.IgnoreNullValues = true;
+                    });
+            
+            services.AddHealthChecks();
+        }
+
+        public static void UseWebApi(this IApplicationBuilder app)
+        {
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
+            });
+        }
+
+        public static void AddSwaggerApiDoc(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevFreela.API", Version = "v1" });
+            });
+        }
+
+        public static void UseSwaggerApiDoc(this IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DevFreela.API v1"));
+        }
+    }
+}
