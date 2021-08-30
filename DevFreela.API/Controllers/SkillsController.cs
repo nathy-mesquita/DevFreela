79ad5ff0eef824e76f712085fabf0343f86a28e4
Nathaly Mesquita <nathaly.mesquita@xpi.com.br>
2021-08-30 01:27:15 -0300

    fix(controller): Use mediator

--- DevFreela.API/Controllers/SkillsController.cs
+++ DevFreela.API/Controllers/SkillsController.cs
@@ -1,20 +1,25 @@
-using DevFreela.Application.Services.Interfaces;
+using MediatR;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using DevFreela.Application.Queries.GetAllSkills;
 
 namespace DevFreela.API.Controllers
 {
     [Route("api/[controller]")]
     public class SkillsController : ControllerBase
     {
-        private readonly ISkillService _skillService;
+        private readonly IMediator _mediator;
 
-        public SkillsController(ISkillService skillService) 
-        => _skillService = skillService;
-
-        [HttpGet]
-        public IActionResult Get()
+        public SkillsController(IMediator mediator) 
         {
-            var skills = _skillService.GetAll();
+            _mediator = mediator;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var getAllSkillsQuery = new GetAllSkillsQuery();
+            var skills = await _mediator.Send(getAllSkillsQuery);
             return Ok(skills);
         }
     }
