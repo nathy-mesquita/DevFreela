4cd7cc3daf48c46dd3a34a3715f3688d263da9cc
Nathaly Mesquita <nathaly.mesquita@xpi.com.br>
2021-09-06 23:51:18 -0300

    feat(): User Repository

---
+++ DevFreela.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using DevFreela.Core.Entities;
+using System.Collections.Generic;
+
+namespace DevFreela.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAllAsync();
+        Task<User> GetByIdAsync(int id);
+    }
+}
