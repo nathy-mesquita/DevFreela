301c03207cc33a7c072b0a651faa9bbe034710ea
Nathaly Mesquita <nathaly.mesquita@xpi.com.br>
2021-09-07 22:05:43 -0300

    fix(): Include Command for UserRepository

--- DevFreela.Infrastructure/Persistence/Repositories/UserRepository.cs
+++ DevFreela.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -13,6 +13,7 @@ namespace DevFreela.Infrastructure.Persistence.Repositories
         public UserRepository(DevFreelaDbContext dbContext) 
             => _dbContext = dbContext;
 
+
         public async Task<List<User>> GetAllAsync()
         {
             return await _dbContext.Users.ToListAsync(); 
@@ -21,6 +22,11 @@ namespace DevFreela.Infrastructure.Persistence.Repositories
         public async Task<User> GetByIdAsync(int id)
         {
         return await _dbContext.Users.SingleOrDefaultAsync(u => u.Id == id);
+        }
+        public async Task AddAsync(User user)
+        {
+            await _dbContext.Users.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
