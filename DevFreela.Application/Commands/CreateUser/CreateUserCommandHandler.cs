1c4afbd33654dab31943f0d712b602e3faca6ca5
Nathaly Mesquita <nathaly.mesquita@xpi.com.br>
2021-09-07 22:05:19 -0300

    fix(): Refactor Command for UserRepository

--- DevFreela.Application/Commands/CreateUser/CreateUserCommandHandler.cs
+++ DevFreela.Application/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -2,22 +2,21 @@ using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
 using DevFreela.Core.Entities;
-using DevFreela.Infrastructure.Persistence;
+using DevFreela.Core.Repositories;
 
 namespace DevFreela.Application.Commands.CreateUser
 {
     public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, int>
     {
-        private readonly DevFreelaDbContext _dbContext;
+        private readonly IUserRepository _userRepository;
 
-        public CreateUserCommandHandler(DevFreelaDbContext dbContext) 
-            => _dbContext = dbContext;
+        public CreateUserCommandHandler(IUserRepository userRepository) 
+            => _userRepository = userRepository;
 
         public async Task<int> Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
             var user = new User(request.FullName, request.Email, request.BirthDate);
-            await _dbContext.Users.AddAsync(user);
-            await _dbContext.SaveChangesAsync();
+            await _userRepository.AddAsync(user);
             return user.Id;
         }
     }
