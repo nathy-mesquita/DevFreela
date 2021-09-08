ca8028d2436f84687b9d5ad180e14dd9469debe5
Nathaly Mesquita <nathaly.mesquita@xpi.com.br>
2021-09-07 22:04:50 -0300

    fix(): Include Command for ProjectRepository

--- DevFreela.Core/Repositories/IProjectRepository.cs
+++ DevFreela.Core/Repositories/IProjectRepository.cs
@@ -1,12 +1,16 @@
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using DevFreela.Core.Entities;
+using System.Collections.Generic;
 
 namespace DevFreela.Core.Repositories
 {
     public interface IProjectRepository
     {
         Task<List<Project>> GetAllAsync();
+        Task<Project> GetDetailsByIdAsync(int id);
         Task<Project> GetByIdAsync(int id);
+        Task AddAsync(Project project);
+        Task SaveChangesAsync();
+        Task AddCommentAsync(ProjectComments projectComments);
     }
 }
--- DevFreela.Infrastructure/Persistence/Repositories/ProjectRepository.cs
+++ DevFreela.Infrastructure/Persistence/Repositories/ProjectRepository.cs
@@ -1,6 +1,6 @@
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using DevFreela.Core.Entities;
+using System.Collections.Generic;
 using DevFreela.Core.Repositories;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,17 +13,34 @@ namespace DevFreela.Infrastructure.Persistence.Repositories
         public ProjectRepository(DevFreelaDbContext dbContext) 
         => _dbContext = dbContext;
 
-
         public async Task<List<Project>> GetAllAsync()
         {
             return await _dbContext.Projects.ToListAsync();
         }
-        public async Task<Project> GetByIdAsync(int id)
+        public async Task<Project> GetDetailsByIdAsync(int id)
         {
             return await _dbContext.Projects
                 .Include(p => p.Client)
                 .Include(p => p.Freelancer) 
                 .SingleOrDefaultAsync(p => p.Id == id);
+        }
+        public async Task<Project> GetByIdAsync(int id)
+        {
+            return await _dbContext.Projects.SingleOrDefaultAsync(p => p.Id == id);
+        }
+        public async Task AddAsync(Project project)
+        {
+            await _dbContext.Projects.AddAsync(project);
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task AddCommentAsync(ProjectComments projectComments)
+        {
+            await _dbContext.ProjectComments.AddAsync(projectComments);
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
