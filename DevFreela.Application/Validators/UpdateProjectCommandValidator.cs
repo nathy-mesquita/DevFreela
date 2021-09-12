2faa0f2249c0eb6338657611e421356a47c664a7
Nathaly Mesquita <nathaly.mesquita@xpi.com.br>
2021-09-11 23:20:39 -0300

    fix(): Add Validations in UpdateProject

--- DevFreela.Application/Validators/UpdateProjectCommandValidator.cs
+++ DevFreela.Application/Validators/UpdateProjectCommandValidator.cs
@@ -11,6 +11,14 @@ namespace DevFreela.Application.Validators
                 .NotEmpty()
                 .NotNull()
                 .WithMessage("O Id é obrigatório!");
+                
+            RuleFor(p => p.Description)
+                .MaximumLength(255)
+                .WithMessage("Tamanho máximo de Descriçao é de 255 caracteres.");
+
+            RuleFor(p => p.Title)
+                .MaximumLength(30)
+                .WithMessage("Tamanho máximo de Título é de 30 caracteres");
         }
     }
 }
