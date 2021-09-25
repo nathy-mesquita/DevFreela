167749873af590e2f45b7f2ca838c67232cdd639
Nathaly Mesquita <nathaly.mesquita@xpi.com.br>
2021-09-25 03:29:38 -0300

    feat(): Roles

---
+++ DevFreela.API/Roles.cs
@@ -0,0 +1,9 @@
+namespace DevFreela.API
+{
+    public static class Roles
+    {
+        public const string Administrador = "DevFreela_Admin";
+        public const string Client = "DevFreela_Client";
+        public const string Freelancer = "DevFreela_Freelancer";
+    }
+}
