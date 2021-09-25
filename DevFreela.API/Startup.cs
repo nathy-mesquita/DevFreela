71916e1a1fd050fb0200419ef85642a18b0dc21f
Nathaly Mesquita <nathaly.mesquita@xpi.com.br>
2021-09-24 22:17:55 -0300

    fix(): AddAuthentication and AddJwtBearer

--- DevFreela.API/Startup.cs
+++ DevFreela.API/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using DevFreela.Infrastructure.Persistence;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DevFreela.API
 {
@@ -21,6 +24,7 @@ namespace DevFreela.API
             services.AddApplication(_configuration);
             services.AddInfrastructure(_configuration);
             services.AddSwaggerApiDoc();
+            AddJwt(services);
 
             var connectionString = _configuration.GetConnectionString("DevFreelaCs");
             services.AddDbContext<DevFreelaDbContext>(
@@ -35,6 +39,27 @@ namespace DevFreela.API
             {
                 app.UseSwaggerApiDoc();
             }
+        }
+
+        public void AddJwt(IServiceCollection services)
+        {
+            services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme) //Schema do Token = Bearer
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true, //Se o Issuer e valido
+                    ValidateAudience = true, //Se o Audience e valido
+                    ValidateLifetime = true, //Se o token ja expirou 
+                    ValidateIssuerSigningKey = true, //Chave de assinatura Sha256
+
+                    IssuerSigningKey = new SymmetricSecurityKey
+                    (Encoding.UTF8.GetBytes(_configuration["Jwt:key"])), //Algoritmos de seguranca
+                    ValidIssuer = _configuration["Jwt:Issuer"], //Onde esta o Issuer da aplicacao
+                    ValidAudience = _configuration["Jwt:Audience"] //Onde esta o Audience da aplicacao
+                };
+            });
         }
     }
 }
